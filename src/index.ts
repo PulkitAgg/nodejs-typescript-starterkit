2349dd298413d930fb19d2193a925bc2ac3f37a9
Pulkit Aggarwal <pulkitagg1010@gmail.com>
2020-05-16 22:28:42 +0530

    Setup environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-
+const DEFAULT_PORT = 5000;
 app.get('*', function (req: any, res: any) {
     res.send('hellow world');
 })
 
 // start server
-app.listen(3000, () => {
-    console.log(`Express server listening on 3000`);
+app.listen(process.env.PORT || DEFAULT_PORT, () => {
+    console.log(`Express server listening on ${process.env.PORT || DEFAULT_PORT} port`);
 });
